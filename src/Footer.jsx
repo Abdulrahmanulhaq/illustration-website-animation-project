a433a467a2a4bfb53afe5ef9c99fda14a9224d03
Abdulrahman ul haq <abdulrahmanhaq.06@gmail.com>
2020-08-18 17:23:46 +0300

    last commit (i think)
    done!!!!!!!!!!!!

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <footer className="w-100 bg-light text-center">
                 <br/>
                 <p> 
-L Ξ G E N D . All Rights Reserved  | Terms and Conditions</p>
+</p>
             </footer>
         </>
     )
